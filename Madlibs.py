6652a55f3fae78487cfa0d96c08a0f3b11b42179
bagiya <bagiya6lakshmi@gmail.com>
2022-05-10 14:41:22 +0200

    Create Madlibs.py

---
+++ Madlibs.py
@@ -0,0 +1,10 @@
+#Program to code Madlibs game - Word guessing
+
+verb = input("Please enter a Verb :")
+adj_1 = input("Please enter an Adjective :")
+adj_2 = input("Please enter an Adjective :")
+
+madlib = f"I like to go {verb} because the weather is so {adj_1} and {adj_2}"
+
+print(madlib)
+
